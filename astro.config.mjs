efe09fc37f660ab464ff9b208ae2b4006dec6603
acmmp <adriancampayo6@gmail.com>
2024-09-05 13:05:59 +0200

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [react()],
-  output: 'server',  // Añade esta línea
+  output: 'server',
+  adapter: vercel(),
   vite: {
     ssr: {
       noExternal: ['react-chartjs-2']
